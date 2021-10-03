bc00a0ad049e71a4b63fec670d9c2a9c6a4177da
Manolo Enriquez <noloenriquez@gmail.com>
2021-10-04 01:23:32 +0800

    removed redux

--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import musicReducer from './reducers/musicSlice';
-
-export default configureStore({
-  reducer: {
-    music: musicReducer,
-  }
-})
