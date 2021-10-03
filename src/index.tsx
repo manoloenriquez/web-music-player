d40c148055a810b2a3b7d657d153afcad51302a4
Manolo Enriquez <noloenriquez@gmail.com>
2021-10-04 01:24:02 +0800

    updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+// import store from './redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
