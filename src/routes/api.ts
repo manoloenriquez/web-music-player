f9370d62d66b22d5e0d684997261538a918368b4
Manolo Enriquez <noloenriquez@gmail.com>
2021-10-04 01:02:38 +0800

    cleaning up directory

--- src/routes/api.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello world!');
-});
-
-export default router;
